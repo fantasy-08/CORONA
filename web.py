3087e542a1ec831e5cc4aff999f032bb9bb8980f
fantasy-08 <59399447+fantasy-08@users.noreply.github.com>
2020-03-10 01:51:11 +0530

    recovery

--- web.py
+++ web.py
@@ -10,6 +10,7 @@ def convert_to_number(strr):
 f=open('C:\\Users\\eshaa\\Desktop\\CORONA\\Data.txt')
 content=f.readlines()
 f.close()
+#OLD DATA COLLECTION
 old_total=''
 for i in content[0][14:]:
     if(i=='\n'):
@@ -22,9 +23,16 @@ for i in content[1][14:]:
         break
     else:
         old_deaths+=i
+old_rec=''
+for i in content[2][11:]:
+    if(i=='\n'):
+        break
+    else:
+        old_rec+=i
+old_rec=int(old_rec)
 old_total=int(old_total)
 old_deaths=int(old_deaths)
-
+ 
 URL = "https://www.worldometers.info/coronavirus/"
 r = requests.get(URL) 
   
@@ -47,8 +55,13 @@ siz='+'
 if(total_cases<old_total):siz=''
 print('TOTAL Deaths- '+str(total_deaths)+'   '+siz,((total_deaths-old_deaths)))
 
+total_rec=(table[2].span.text)
+total_rec=convert_to_number(total_rec)
+siz='+'
+print('Recovered- '+str(total_rec)+'   '+siz,((total_rec-old_rec)))
 
 f=open('C:\\Users\\eshaa\\Desktop\\CORONA\\Data.txt','w')
 f.write('Total Cases : '+str(total_cases)+'\n')
 f.write('Total Deaths : '+str(total_deaths)+'\n')
+f.write('Recovered : '+str(total_rec)+'\n')
 f.close()
